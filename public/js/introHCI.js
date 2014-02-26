393dcbb39a281c219272b2ed9f0c8f624907a59f
Sierra Kaplan-Nelson <sierrakaplan-nelson@dn0a229745.sunet>
2014-02-26 12:24:10 -0800

    Added timing

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,12 +1,19 @@
 'use strict';
 
+var startTime;
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+	startTime = new Date().getTime();
 });
 
 $(".likeBtn").click(function() {
 	ga("send", "event", "like", "click");
+	var endTime = new Date().getTime();
+	var timeSpent = endTime - startTime;
+
+	ga('send', 'timing', 'jQuery', 'Like a Project', timeSpent);
 })
 
 /*
